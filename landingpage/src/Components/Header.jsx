2336c82e9c3978ed2e0f4f0f82b916b792af4480
Babatunde99 <adeboyebabatunde93@gmail.com>
2022-06-06 14:45:50 +0100

    mobile view of navbar completed

--- landingpage/src/Components/Header.jsx
+++ landingpage/src/Components/Header.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import style from '../sass/style.css'
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from "react-icons/fa"
+import '../sass/style.css'
 
 const Header = () => {
+    const [isMobile, setMobile] = useState(false);
+
+    const handleMobile = () => {
+        setMobile((prev) => !prev);
+    }
+    console.log(isMobile);
+
     return (
         <div className='navBar alignment'>
             <div className='logo'>
                 <h2>Corpers <span>lodge</span></h2>
             </div>
-            <ul>
+            <ul className={`${isMobile ? 'link' : 'ul'}`}>
                 <li>Home</li>
                 <li>Listing</li>
                 <li>About Us</li>
                 <li>Contact</li>
                 <li>Login</li>
-            <button>Sign Up</button>
+                <button>Sign Up</button>
             </ul>
+            <span className='switcher' onClick={handleMobile}>
+                {isMobile ? <FaTimes /> : <FaBars />}
+            </span>
         </div>
     )
 }
